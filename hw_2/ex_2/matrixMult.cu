4b8d793dec04a6a80c50732635e926332674361f
Jonah Ekelund <73965092+Jonah-E@users.noreply.github.com>
2023-12-01 10:52:40 +0100

    A2: Add code for matrixMult.cu

--- hw_2/ex_2/matrixMult.cu
+++ hw_2/ex_2/matrixMult.cu
@@ -1,21 +1,104 @@
-
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/time.h>
 
 #define DataType double
 
-// Compute C = A * B
-__global__ void gemm(DataType *A, DataType *B, DataType *C, int numARows,
-                      int numAColumns, int numBRows, int numBColumns){
-  //@@ Insert code to implement matrix multiplication here
+#define TPW 32
+#define BLOCK_X_SIZE TPW
+#define BLOCK_Y_SIZE TPW
 
+/* Support function to generate a random number in a given range.
+ * Based on the solution presented in the following forum thread:
+ * http://ubuntuforums.org/showthread.php?t=1717717&p=10618266#post10618266*/
+static DataType randfrom(DataType min, DataType max) {
+  DataType range = (max - min);
+  DataType div = RAND_MAX / range;
+  return min + (rand() / div);
 }
 
+/* Populate a given matrix memory location with values from a given range.*/
+static void generateRandMatrix(DataType *matrix, int rows, int columns, DataType min,
+                        DataType max) {
+  srand(time(NULL));
+
+  for (int i = 0; i < rows; ++i) {
+    for (int j = 0; j < columns; ++j) {
+      matrix[(i * columns) + j] = randfrom(min, max);
+    }
+  }
+}
+
+/*Get the current CPU time in seconds as a double.*/
+static double getCpuSeconds(void) {
+  struct timeval tp;
+  gettimeofday(&tp, NULL);
+  return ((double)tp.tv_sec + (double)tp.tv_usec * 1.e-6);
+}
+
+/* Function to calculate the Euclidian norm on the difference between two matrices.*/
+static DataType euclidianNormTwoMatrices(DataType *matrixA, DataType *matrixB,
+                                  int rows, int columns) {
+  DataType diffEu = 0;
+  DataType *result = (DataType *)malloc(sizeof(DataType) * rows * columns);
+
+  for (int i = 0; i < rows; ++i) {
+    for (int j = 0; j < columns; ++j) {
+      result[i * columns + j] =
+          matrixA[i * columns + j] - matrixB[i * columns + j];
+      diffEu += result[i * columns + j] * result[i * columns + j];
+    }
+  }
+  diffEu = sqrt(diffEu);
+
+  free(result);
+  return diffEu;
+}
+
+/* CUDA Kernel to perform the GEMM, designed to launch one thread per element in the C
+ * output matrix.*/
+__global__ void gemmGPU2D(DataType *A, DataType *B, DataType *C, int numARows,
+                          int numAColumns, int numBRows, int numBColumns) {
+  const int idx = threadIdx.x + blockDim.x * blockIdx.x;
+  const int idy = threadIdx.y + blockDim.y * blockIdx.y;
+
+  if (idx < numBColumns && idy < numARows) {
+    DataType result = 0.0;
+
+    /* Do the calculation for the element idx, idy in the output matrix C.*/
+    for (int k = 0; k < numAColumns; ++k) {
+      result += A[idy * numAColumns + k] * B[k * numBColumns + idx];
+    }
+    C[idy * numBColumns + idx] = result;
+  }
+}
+
+/* CPU function to perform the GEMM, C = A x B */
+static void gemmCPU(DataType *A, DataType *B, DataType *C, int numARows,
+             int numAColumns, int numBRows, int numBColumns) {
+  for (int idy = 0; idy < numARows; ++idy) {
+    for (int idx = 0; idx < numBColumns; ++idx) {
+      for (int k = 0; k < numBRows; ++k) {
+        C[idy * numBColumns + idx] +=
+            A[idy * numAColumns + k] * B[k * numBColumns + idx];
+      }
+    }
+  }
+}
+
+static void usage(char *prog) {
+  printf("Usage: %s <numARows> <numAColumns> <numBColumns>\n"
+         "Where all <..> are integer larger than zero.\n",
+         prog);
+}
+
+#define ARGV_EXPECTED_LENGTH 4
 int main(int argc, char **argv) {
-  
-  DataType *hostA; // The A matrix
-  DataType *hostB; // The B matrix
-  DataType *hostC; // The output C matrix
+  double time_start, time_elapsed;
+  cudaError_t deviceError;
+  DataType *hostA;     // The A matrix
+  DataType *hostB;     // The B matrix
+  DataType *hostC;     // The output C matrix
   DataType *resultRef; // The reference result
   DataType *deviceA;
   DataType *deviceB;
@@ -27,39 +110,116 @@ int main(int argc, char **argv) {
   int numCRows;
   int numCColumns;
 
-  //@@ Insert code below to read in numARows, numAColumns, numBColumns from args
+  if (argc != ARGV_EXPECTED_LENGTH) {
+    usage(argv[0]);
+    return 1;
+  }
 
-  printf("Input matrix dim (%d x %d) (%d x %d) (%d x %d)\n", numARows, numAColumns, numBRows, numBColumns, numCRows, numCColumns);
-  
-  //@@ Insert code below to allocate Host memory for input and output
+  numARows = atoi(argv[1]);
+  numAColumns = atoi(argv[2]);
+  numBColumns = atoi(argv[3]);
 
-  
-  //@@ Insert code below to initialize hostA and hostB to random numbers, and create reference result in CPU
+  if (numARows < 1 || numAColumns < 1 || numBColumns < 1) {
+    usage(argv[0]);
+    return 1;
+  }
 
+  numBRows = numAColumns;
+  numCRows = numARows;
+  numCColumns = numBColumns;
 
-  //@@ Insert code below to allocate GPU memory here
+  printf("Input matrix dim (%d x %d) (%d x %d) (%d x %d)\n", numARows,
+         numAColumns, numBRows, numBColumns, numCRows, numCColumns);
 
+  hostA = (DataType *)malloc(sizeof(DataType) * numARows * numAColumns);
+  hostB = (DataType *)malloc(sizeof(DataType) * numBRows * numBColumns);
+  hostC = (DataType *)malloc(sizeof(DataType) * numCRows * numCColumns);
 
-  //@@ Insert code to below to Copy memory to the GPU here
+  time_start = getCpuSeconds();
+  generateRandMatrix(hostA, numARows, numAColumns, 0, 1);
+  generateRandMatrix(hostB, numBRows, numBColumns, 0, 1);
+  memset(hostC, 0, sizeof(DataType) * numARows * numBColumns);
+  time_elapsed = getCpuSeconds() - time_start;
+  printf("host matrix init time: %lf s\n", time_elapsed);
 
+  time_start = getCpuSeconds();
+  gemmCPU(hostA, hostB, hostC, numARows, numAColumns, numBRows, numBColumns);
+  time_elapsed = getCpuSeconds() - time_start;
+  printf("gemmCPU time: %lf s\n", time_elapsed);
 
-  //@@ Initialize the grid and block dimensions here
+  /* Allocate the needed Device memory*/
+  time_start = getCpuSeconds();
+  deviceError = cudaMalloc(&deviceA, sizeof(DataType) * numARows * numAColumns);
+  if (deviceError != cudaSuccess) {
+    printf("Error when running GPU: %s (%d)\n", cudaGetErrorString(deviceError),
+           deviceError);
+  }
+  deviceError = cudaMalloc(&deviceB, sizeof(DataType) * numBRows * numBColumns);
+  if (deviceError != cudaSuccess) {
+    printf("Error when running GPU: %s (%d)\n", cudaGetErrorString(deviceError),
+           deviceError);
+  }
+  deviceError = cudaMalloc(&deviceC, sizeof(DataType) * numCRows * numCColumns);
+  if (deviceError != cudaSuccess) {
+    printf("Error when running GPU: %s (%d)\n", cudaGetErrorString(deviceError),
+           deviceError);
+  }
 
+  /* Copy data to device.*/
+  deviceError =
+      cudaMemcpy(deviceA, hostA, sizeof(DataType) * numARows * numAColumns,
+                 cudaMemcpyHostToDevice);
+  if (deviceError != cudaSuccess) {
+    printf("Error when running GPU: %s (%d)\n", cudaGetErrorString(deviceError),
+           deviceError);
+  }
+  deviceError =
+      cudaMemcpy(deviceB, hostB, sizeof(DataType) * numBRows * numBColumns,
+                 cudaMemcpyHostToDevice);
+  if (deviceError != cudaSuccess) {
+    printf("Error when running GPU: %s (%d)\n", cudaGetErrorString(deviceError),
+           deviceError);
+  }
+  deviceError =
+      cudaMemset(deviceC, 0, sizeof(DataType) * numCRows * numCColumns);
+  if (deviceError != cudaSuccess) {
+    printf("Error when running GPU: %s (%d)\n", cudaGetErrorString(deviceError),
+           deviceError);
+  }
 
-  //@@ Launch the GPU Kernel here
+  dim3 block(BLOCK_X_SIZE, BLOCK_Y_SIZE);
+  dim3 grid((numCRows + BLOCK_X_SIZE - 1) / BLOCK_X_SIZE,
+            (numCColumns + BLOCK_Y_SIZE - 1) / BLOCK_Y_SIZE);
 
+  time_start = getCpuSeconds();
+  gemmGPU2D<<<grid, block>>>(deviceA, deviceB, deviceC, numARows, numAColumns,
+                             numBRows, numBColumns);
+  cudaDeviceSynchronize();
+  time_elapsed = getCpuSeconds() - time_start;
 
-  //@@ Copy the GPU memory back to the CPU here
+  deviceError = cudaGetLastError();
+  if (deviceError != cudaSuccess) {
+    printf("Error when running GPU: %s (%d)\n", cudaGetErrorString(deviceError),
+           deviceError);
+  } else {
+    printf("gemmGPU time: %lf s\n", time_elapsed);
 
+    resultRef = (DataType *)malloc(sizeof(DataType) * numCRows * numCColumns);
+    cudaMemcpy(resultRef, deviceC, sizeof(DataType) * numCRows * numCColumns,
+               cudaMemcpyDeviceToHost);
 
-  //@@ Insert code below to compare the output with the reference
+    DataType diffEu =
+        euclicianNormTwoMatrices(resultRef, hostC, numCRows, numCColumns);
+    printf("Euclidian norm of the difference: %lf", diffEu);
+  }
 
+  cudaFree(deviceA);
+  cudaFree(deviceB);
+  cudaFree(deviceC);
 
-  //@@ Free the GPU memory here
-
-
-  //@@ Free the CPU memory here
-
-
+  free(hostA);
+  free(hostB);
+  free(hostC);
+  free(resultRef);
   return 0;
 }
