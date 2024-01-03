0b746efac4359eeb30546127e7315fd999f32f6b
Jonah Ekelund <73965092+Jonah-E@users.noreply.github.com>
2024-01-03 20:19:16 +0100

    A4: Update vectorAdd to use segementation

--- hw_4/ex_2/vectorAdd-stream.cu
+++ hw_4/ex_2/vectorAdd-stream.cu
@@ -16,11 +16,11 @@ static DataType randfrom(DataType min, DataType max) {
 }
 
 /* Populate a given vector memory location with values from a given range.*/
-static void generateRandVector(DataType *matrix, int lenght, DataType min,
-                        DataType max) {
+static void generateRandVector(DataType *matrix, int length, DataType min,
+                               DataType max) {
   srand(time(NULL));
 
-  for (int i = 0; i < lenght; ++i) {
+  for (int i = 0; i < length; ++i) {
     matrix[i] = randfrom(min, max);
   }
 }
@@ -32,13 +32,14 @@ static double getCpuSeconds(void) {
   return ((double)tp.tv_sec + (double)tp.tv_usec * 1.e-6);
 }
 
-/* Function to calculate the Euclidian norm on the difference between two vectors.*/
+/* Function to calculate the Euclidian norm on the difference between two
+ * vectors.*/
 static DataType euclicianNormTwoVectors(DataType *vectorA, DataType *vectorB,
-                                 int lenght) {
+                                        int length) {
   DataType diffEu = 0;
-  DataType *result = (DataType *)malloc(sizeof(DataType) * lenght);
+  DataType *result = (DataType *)malloc(sizeof(DataType) * length);
 
-  for (int i = 0; i < lenght; ++i) {
+  for (int i = 0; i < length; ++i) {
     result[i] = vectorA[i] - vectorB[i];
     diffEu += result[i] * result[i];
   }
@@ -55,7 +56,6 @@ __global__ void vecAddGPU(DataType *out, DataType *in1, DataType *in2,
 
   if (idx >= len)
     return;
-
   out[idx] = in1[idx] + in2[idx];
 }
 
@@ -67,16 +67,20 @@ static void vecAddCPU(DataType *out, DataType *in1, DataType *in2, int len) {
 }
 
 static void usage(char *prog) {
-  printf("Usage: %s <inputLength>\n"
-         "Where <inputLength> is an integer larger than zero.\n",
-         prog);
+  printf("Usage: %s <inputLength> <segmentSize>\n"
+         "Where <inputLength> is an integer larger than zero\n"
+         "and segmentSize is an integer divisable by %d.\n",
+         prog, TPW);
 }
 
 int main(int argc, char **argv) {
 
   double time_start, time_elapsed;
+  cudaError_t deviceError;
 
   int inputLength;
+  int segmentSize;
+  int numberOfSegments;
   DataType *hostInput1;
   DataType *hostInput2;
   DataType *hostOutput;
@@ -85,22 +89,50 @@ int main(int argc, char **argv) {
   DataType *deviceInput2;
   DataType *deviceOutput;
 
-  if (argc != 2) {
+  if (argc != 3) {
     usage(argv[0]);
     return 1;
   }
 
   inputLength = atoi(argv[1]);
+  segmentSize = atoi(argv[2]);
   if (inputLength < 1) {
     usage(argv[0]);
     return 1;
   }
 
-  printf("inputLength, cpu_exec (s), host_to_device (s), gpu_exec (s), device_to_host (s), differece\n");
-  printf("%d,", inputLength);
+  if ((segmentSize % TPW) != 0) {
+    usage(argv[0]);
+    return 1;
+  }
 
-  hostInput1 = (DataType *)malloc(sizeof(DataType) * inputLength);
-  hostInput2 = (DataType *)malloc(sizeof(DataType) * inputLength);
+  numberOfSegments = (inputLength + (segmentSize - 1)) / segmentSize;
+
+  printf("inputLength, segmentSize, cpu_exec (s), gpu_exec (s), differece\n");
+  printf("%d,%d,", inputLength, segmentSize);
+
+  deviceError =
+      cudaHostAlloc((void **)&hostInput1, sizeof(DataType) * inputLength,
+                    cudaHostAllocDefault);
+  if (deviceError != cudaSuccess) {
+    printf("CUDA Error: %s (%d)\n", cudaGetErrorString(deviceError),
+           deviceError);
+  }
+
+  deviceError =
+      cudaHostAlloc((void **)&hostInput2, sizeof(DataType) * inputLength,
+                    cudaHostAllocDefault);
+  if (deviceError != cudaSuccess) {
+    printf("CUDA Error: %s (%d)\n", cudaGetErrorString(deviceError),
+           deviceError);
+  }
+  deviceError =
+      cudaHostAlloc((void **)&resultRef, sizeof(DataType) * inputLength,
+                    cudaHostAllocDefault);
+  if (deviceError != cudaSuccess) {
+    printf("CUDA Error: %s (%d)\n", cudaGetErrorString(deviceError),
+           deviceError);
+  }
   hostOutput = (DataType *)malloc(sizeof(DataType) * inputLength);
 
   generateRandVector(hostInput1, inputLength, 0, 1);
@@ -111,54 +143,83 @@ int main(int argc, char **argv) {
   time_elapsed = getCpuSeconds() - time_start;
   printf("%lf,", time_elapsed);
 
-  time_start = getCpuSeconds();
-  cudaMalloc(&deviceInput1, sizeof(DataType) * inputLength);
-  cudaMalloc(&deviceInput2, sizeof(DataType) * inputLength);
-  cudaMalloc(&deviceOutput, sizeof(DataType) * inputLength);
-
-  cudaMemcpy(deviceInput1, hostInput1, sizeof(DataType) * inputLength,
-             cudaMemcpyHostToDevice);
-  cudaMemcpy(deviceInput2, hostInput2, sizeof(DataType) * inputLength,
-             cudaMemcpyHostToDevice);
-  cudaMemset(deviceOutput, 0, sizeof(DataType) * inputLength);
-  time_elapsed = getCpuSeconds() - time_start;
-  printf("%lf,", time_elapsed);
+  deviceError = cudaMalloc(&deviceInput1, sizeof(DataType) * inputLength);
+  if (deviceError != cudaSuccess) {
+    printf("CUDA Error: %s (%d)\n", cudaGetErrorString(deviceError),
+           deviceError);
+  }
+  deviceError = cudaMalloc(&deviceInput2, sizeof(DataType) * inputLength);
+  if (deviceError != cudaSuccess) {
+    printf("CUDA Error: %s (%d)\n", cudaGetErrorString(deviceError),
+           deviceError);
+  }
+  deviceError = cudaMalloc(&deviceOutput, sizeof(DataType) * inputLength);
+  if (deviceError != cudaSuccess) {
+    printf("CUDA Error: %s (%d)\n", cudaGetErrorString(deviceError),
+           deviceError);
+  }
 
   dim3 block(TPW, 1, 1);
-  dim3 grid((inputLength + TPW - 1) / TPW, 1, 1);
+  dim3 grid((segmentSize + TPW - 1) / TPW, 1, 1);
+
+  cudaStream_t streams[numberOfSegments];
+  int inputLength_stream = segmentSize;
+  int stream_id = 0;
+
   time_start = getCpuSeconds();
-  vecAddGPU<<<grid, block>>>(deviceOutput, deviceInput1, deviceInput2,
-                             inputLength);
-  cudaDeviceSynchronize();
+  for (int i = 0; i < inputLength; i += segmentSize) {
+    cudaStreamCreate(&streams[stream_id]);
+
+    if ((i + segmentSize) > inputLength) {
+      inputLength_stream = inputLength - i;
+    }
+
+    cudaMemcpyAsync(&deviceInput1[i], &hostInput1[i],
+                    sizeof(DataType) * inputLength_stream, cudaMemcpyHostToDevice,
+                    streams[stream_id]);
+
+    cudaMemcpyAsync(&deviceInput2[i], &hostInput2[i],
+                    sizeof(DataType) * inputLength_stream, cudaMemcpyHostToDevice,
+                    streams[stream_id]);
+
+    cudaMemsetAsync(&deviceOutput[i], 0, sizeof(DataType) * inputLength_stream,
+                    streams[stream_id]);
+
+    vecAddGPU<<<grid, block, 0, streams[stream_id]>>>(
+        &deviceOutput[i], &deviceInput1[i], &deviceInput2[i],
+        inputLength_stream);
+
+    cudaMemcpyAsync(&resultRef[i], &deviceOutput[i],
+                    sizeof(DataType) * inputLength_stream,
+                    cudaMemcpyDeviceToHost, streams[stream_id]);
+    ++stream_id;
+  }
+
+  for (int i = 0; i < numberOfSegments; ++i) {
+    cudaStreamSynchronize(streams[i]);
+    cudaStreamDestroy(streams[i]);
+  }
   time_elapsed = getCpuSeconds() - time_start;
   printf("%lf,", time_elapsed);
 
-  cudaError_t deviceError = cudaGetLastError();
+  deviceError = cudaGetLastError();
   if (deviceError != cudaSuccess) {
     printf("Error when running GPU: %s (%d)\n", cudaGetErrorString(deviceError),
            deviceError);
-  } else {
-    resultRef = (DataType *)malloc(sizeof(DataType) * inputLength);
-
-    time_start = getCpuSeconds();
-    cudaMemcpy(resultRef, deviceOutput, sizeof(DataType) * inputLength,
-               cudaMemcpyDeviceToHost);
-    time_elapsed = getCpuSeconds() - time_start;
-    printf("%lf,", time_elapsed);
-
-    DataType diffEu =
-        euclicianNormTwoVectors(resultRef, hostOutput, inputLength);
-    printf("%lf\n", diffEu);
   }
+
+  DataType diffEu = euclicianNormTwoVectors(resultRef, hostOutput, inputLength);
+  printf("%lf\n", diffEu);
 
   cudaFree(deviceInput1);
   cudaFree(deviceInput2);
   cudaFree(deviceOutput);
 
-  free(hostInput1);
-  free(hostInput2);
+  cudaFreeHost(hostInput1);
+  cudaFreeHost(hostInput2);
+  cudaFreeHost(resultRef);
+
   free(hostOutput);
-  free(resultRef);
 
   return 0;
 }
